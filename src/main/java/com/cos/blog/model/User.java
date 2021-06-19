b0de4fb2808a51a013d9df57415c945dabc49e55
yhkim <yhkim@hunesion.com>
2021-06-19 22:55:14 +0900

    [18강] User 테이블 생성(JPA이용)
    @Entity
    
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Column(nullable = false, length = 30)
    @ColumnDefault("'user'")
    @ColumnDefault("'user'")

---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,34 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity // User class가 마리아DB에 테이블로 생성된다.
+public class User {
+	@Id // Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따른다.
+	private int id; // auto_increment
+	
+	@Column(nullable = false, length = 30)
+	private String username;
+	
+	@Column(nullable = false, length=256)
+	private String password;
+	
+	@Column(nullable = false, length = 50)
+	private String email;
+	
+	@ColumnDefault("'user'")
+	private String role; // admin/user/manager
+
+	@CreationTimestamp
+	private Timestamp createDate;	
+}
