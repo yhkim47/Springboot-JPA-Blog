c88e0d1f26992555b148f3e979fd417a84e1b9d6
yhkim <yhkim@hunesion.com>
2021-06-19 17:11:27 +0900

    [16강] lombok 적용 및 @Builder 사용
    @Data
    @NoArgsConstructor
    @Builder

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -10,6 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HttpControllerTest {
+	private static final String TAG = "HttpControllerTest:";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().id(1).password(1234).username("yhkim").email("yhkim@gmail.com").build();
+		System.out.println(TAG + "lombok:" + m.getPassword());
+		m.setPassword(5678);
+		System.out.println(TAG + "lombok:" + m.getPassword());
+		return "lombok test 완료";
+	}
+	//
 	@GetMapping("/http/get1")
 	public String httpGet1(@RequestParam int id, @RequestParam String username, @RequestParam int password, @RequestParam String email) {
 		return "Http Get1, " + id + ",'" + username + ",'" + password + ",'" + email;
