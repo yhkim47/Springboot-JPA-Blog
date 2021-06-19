0d459f664898ce4e6fbaee91ad338101d418f0b7
yhkim <yhkim@hunesion.com>
2021-06-20 00:02:42 +0900

    [22강] Board 테이블 연관관계 처리
    @ManyToOne(fetch = FetchType.EAGER)
    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -38,11 +41,13 @@ public class Board {
 	@ColumnDefault("0")
 	private int count;
 	
-	@ManyToOne // Many=Board, One=User
+	@ManyToOne(fetch = FetchType.EAGER) // Many=Board, One=User
 	@JoinColumn(name = "userId")
 	private User user;
 
 	@CreationTimestamp
 	private Timestamp createDate;
-	
+
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // DB에 컬럼을 만들지 마세요.
+	private List<Reply> reply;
 }
