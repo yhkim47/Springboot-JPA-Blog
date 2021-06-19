2990912bc6a214bed12048019fe78fa321b598dc
yhkim <yhkim@hunesion.com>
2021-06-19 23:16:57 +0900

    [20강] Board 테이블 생성(JPA이용)
    @Lob
    @ManyToOne // Many=Board, One=User
    @JoinColumn(name = "userId")

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,39 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob
+	private String content;
+	
+	@ColumnDefault("0")
+	private int count;
+	
+	@ManyToOne // Many=Board, One=User
+	@JoinColumn(name = "userId")
+	private User user;
+
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+}
