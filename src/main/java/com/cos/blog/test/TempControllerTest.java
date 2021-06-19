7e35e9e537171c528b16e3baa7c6f3597517f8cd
yhkim <yhkim@hunesion.com>
2021-06-19 19:06:36 +0900

    [17강] jsp 처리 시작

---
+++ src/main/java/com/cos/blog/test/TempControllerTest.java
@@ -0,0 +1,27 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+	@GetMapping("/temp/home")
+	public String tempHome() {
+		return "/home.html";
+	}
+
+	@GetMapping("/temp/img")
+	public String tempImg() {
+		return "/a.png";
+	}
+
+	@GetMapping("/temp/jsp")
+	public String tempJsp() {
+		return "/test.jsp";
+	}
+
+	@GetMapping("/temp/realjsp")
+	public String tempRealJsp() {
+		return "test";
+	}
+}
