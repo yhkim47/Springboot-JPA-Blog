76361c82c9e935f19e233adb984dd59447e9a323
yhkim <yhkim@hunesion.com>
2021-06-23 00:05:11 +0900

    [38강] 회원가입 ajax요청 및 응답 api 생성, ResponseDto 처리

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -13,7 +13,20 @@ let index = {
 			email: $("#email").val()
 		};
 		//console.log(data);
-		$.ajax().done().fail(); // ajax 통신을 이용해서 data를 JSON으로 변경하여 insert 요청
+		// ajax 통신을 이용해서 data를 JSON으로 변경하여 insert 요청
+		$.ajax({
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data), // http body 데이터
+			contentType: "application/json; charset=utf-8", // 요청 데이터의 타입
+			dataType: "json" // 응답 데이터의 타입, 응답데이터는 javascript의 오브젝트로 변경되어 수신된다.
+		}).done(function(resp) {
+			console.log("resp", resp);
+			alert("회원가입완료");
+			//location.href = "/blog";
+		}).fail(function(error) {
+			alert("회원가입실패" + JSON.stringify(error));
+		}); 
 	}
 };
 
